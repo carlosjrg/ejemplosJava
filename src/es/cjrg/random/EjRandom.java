90d1a165196a85da052204e2de529228ec494904
Carlos Real <creal86@gmail.com>
2016-04-14 18:32:41 +0200

    Añadido ejemplo Random

---
+++ src/es/cjrg/random/EjRandom.java
@@ -0,0 +1,22 @@
+package es.cjrg.random;
+
+import java.util.Random;
+
+public class EjRandom {
+
+	public static void main(String[] args) {
+		
+		int randomNum;
+		
+		//OPCION 1
+		Random aleatorio = new Random();
+	    randomNum = aleatorio.nextInt(5);
+	    System.out.println(randomNum);
+
+	    //OPCION 2
+	    randomNum =  (int)(Math.random()*5); 
+	    System.out.println(randomNum);
+
+	}
+
+}
